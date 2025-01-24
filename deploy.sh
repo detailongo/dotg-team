b5091b38f1d1431f807f05f6d3dd855a70c4c9ab
Levi Taylor <levi@detailongo.com>
2025-01-24 20:23:30 +0000

    lilsecrets in the deploy so I can run faster

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,9 @@ gcloud run deploy dotg \
   --image gcr.io/$(gcloud config get-value project)/dotg \
   --platform managed \
   --region us-central1 \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --set-env-vars "NEXT_PUBLIC_UPLOADTHING_APP_ID=7dzxf2x6zw" \
+  --set-env-vars "UPLOADTHING_SECRET=sk_live_51e673baa880ae4773e4d01a679a6aa073d96e12cdd9e66255f972a6bc3ba39d" \
+  --set-env-vars "UPLOADTHING_TOKEN=eyJhcGlLZXkiOiJza19saXZlXzUxZTY3M2JhYTg4MGFlNDc3M2U0ZDAxYTY3OWE2YWEwNzNkOTZlMTJjZGQ5ZTY2MjU1Zjk3MmE2YmMzYmEzOWQiLCJhcHBJZCI6IjdkenhmMng2enciLCJyZWdpb25zIjpbInNlYTEiXX0=" \
 
 echo "Deployment complete!"
